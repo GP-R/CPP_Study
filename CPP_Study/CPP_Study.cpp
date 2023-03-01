81efc5bdb85902f7b6361015eca573afa6b2e7d9
GP-R <seonwoo3644@naver.com>
2023-03-01 23:36:39 +0900

    modern c++ #1

--- CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study.cpp
@@ -2,52 +2,10 @@
 using namespace std;
 #include <vector>
 
-bool findNum(vector<int>& v, int num)
-{
-	for (vector<int>::iterator it = v.begin(); it != v.end(); ++it)
-	{
-		if (num == (*it))
-		{
-			return true;
-		}
-	}
-	return false;
-}
-
-int isDivEleven(vector<int>& v)
-{
-	int count = 0;
-	for (vector<int>::iterator it = v.begin(); it != v.end(); ++it)
-	{
-		if ((*it)%11 == 0)
-		{
-			count++;
-		}
-	}
-	return count;
-}
 
 int main()
 {
-	srand(static_cast<unsigned int>(time(nullptr)));
-	vector<int> v;
-	for (int i = 1; i < 100; i++)
-	{
-		//int num = rand() % 100;
-		v.push_back(i);
-	}
-	int input;
-	cin >> input;
-	bool result = findNum(v, input);
-	cout << result << endl;
-	int elecount = isDivEleven(v);
-	cout << elecount << endl;
-
-	for (vector<int>::iterator it = v.begin(); it != v.end(); ++it)
-	{
-		(*it) = (*it) * 3;
-		cout << (*it) << endl;
-	}
+	
 	
 	return 0;
 }
